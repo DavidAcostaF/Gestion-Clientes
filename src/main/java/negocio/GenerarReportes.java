d44bb499f94ed9bf3063ac8e0192129db4b85240
rsarayos <raul.sauceda@potros.itson.edu.mx>
2024-09-09 14:27:43 -0700

    reporte
    Ajuste

--- src/main/java/negocio/GenerarReportes.java
+++ src/main/java/negocio/GenerarReportes.java
@@ -25,13 +25,14 @@ import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
+import net.sf.jasperreports.view.JasperViewer;
 
 /**
  *
  * @author alex_
  */
 public class GenerarReportes {
-    
+
     public void generarReporte(List<ClienteDTO> listaClientes) {
         // Crear un JRBeanCollectionDataSource con la lista de TramiteReporteDTO
         JRBeanCollectionDataSource itemsJRBean = new JRBeanCollectionDataSource(listaClientes);
@@ -39,42 +40,18 @@ public class GenerarReportes {
         Map<String, Object> parameters = new HashMap<>();
         parameters.put("CollectionBeanParam", itemsJRBean);
 
-        // Configuración del JFileChooser para seleccionar la ubicación y nombre del archivo
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setDialogTitle("Guardar Reporte");
-        fileChooser.setFileFilter(new FileNameExtensionFilter("Archivos PDF", "pdf"));
+        // Cargar el diseño del reporte desde el archivo "reporteClientes.jrxml"
+        try (InputStream input = new FileInputStream(new File("reporteClientes.jrxml"))) {
+            JasperDesign jasperDesign = JRXmlLoader.load(input);
+            // Compilar el reporte
+            JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
+            // Llenar el reporte con los datos y parámetros proporcionados
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
 
-        // Mostrar el diálogo para guardar el archivo
-        int userSelection = fileChooser.showSaveDialog(null);
-
-        // Si el usuario selecciona guardar
-        if (userSelection == JFileChooser.APPROVE_OPTION) {
-            File fileToSave = fileChooser.getSelectedFile();
-            String filePath = fileToSave.getAbsolutePath();
-
-            // Asegurar que la extensión del archivo sea .pdf
-            if (!filePath.endsWith(".pdf")) {
-                filePath += ".pdf";
-            }
-
-            // Cargar el diseño del reporte desde el archivo "Tramites.jrxml"
-            try (InputStream input = new FileInputStream(new File("reporteClientes.jrxml"))) {
-                JasperDesign jasperDesign = JRXmlLoader.load(input);
-                // Compilar el reporte
-                JasperReport jasperReport = JasperCompileManager.compileReport(jasperDesign);
-                // Llenar el reporte con los datos y parámetros proporcionados
-                JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, new JREmptyDataSource());
-
-                // Exportar el reporte a un archivo PDF
-                try (
-                        OutputStream outputStream = new FileOutputStream(new File(filePath))) {
-                    JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
-                }
-
-            } catch (Exception ex) {
-            }
-            JOptionPane.showMessageDialog(null, "Archivo guardado", "Info", JOptionPane.INFORMATION_MESSAGE);
-        } else if (userSelection == JFileChooser.CANCEL_OPTION) {
+            // Mostrar el reporte en el visor
+            JasperViewer.viewReport(jasperPrint, false); 
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
     }
 
